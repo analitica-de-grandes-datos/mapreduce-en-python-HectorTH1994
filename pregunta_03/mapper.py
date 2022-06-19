44b836005b931747b116c23c7e2e4d56e58ea4f5
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-19 12:50:30 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -7,11 +7,9 @@ import sys
 elements = []
 if __name__ == '__main__':
 
-    def take_element(element):
-        return element.split(",")[1]
+
     for line in sys.stdin:
-        elements.append(line)
-    else:
-        elements = sorted(elements, key = take_element)
-        for element in elements:
-            sys.stdout.write(element)
+        lista=line.split(",")[1]
+
+        sys.stdout.write(line)
+            

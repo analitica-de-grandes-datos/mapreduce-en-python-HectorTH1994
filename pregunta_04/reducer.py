3bf1b10dd21600062563a60436cb61b308fd1581
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-19 13:04:54 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
             total += val
         else:
             if curkey is not None:
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
+                sys.stdout.write("{},{}\n".format(curkey, total))
 
             curkey = key
             total = val
 
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    sys.stdout.write("{},{}\n".format(curkey, total))

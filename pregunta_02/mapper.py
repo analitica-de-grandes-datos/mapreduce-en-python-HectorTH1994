e7d6591ea9c57d247542eff5369ac0bbd1815bde
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-18 20:05:34 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,26 @@
 #
-# >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        word=line.split(",")[3]+";"+line.split(",")[4]
+            #
+            # escribe al flujo estandar de salida
+            #
+        sys.stdout.write("{}\n".format(word))

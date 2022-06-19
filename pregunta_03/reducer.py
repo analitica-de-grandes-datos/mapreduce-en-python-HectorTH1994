71494ccfbb13fc6e2cafabf2a8a7626e8d01f5ea
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-19 12:37:55 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -2,17 +2,16 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 # reference https://docs.python.org/3/howto/sorting.html
+#! /usr/bin/python3
 import sys
-
 elements = []
+if __name__ == '__main__':
 
-def take_element(element):
-    return element.split(",")[1]
-
-for row in sys.stdin:
-    elements.append(row)
-else:
-    elements = sorted(elements, key = take_element)
-    
-    for element in elements:
-        sys.stdout.write(element)
+    def take_element(element):
+        return element.split(",")[1]
+    for line in sys.stdin:
+        elements.append(line)
+    else:
+        elements = sorted(elements, key = take_element)
+        for element in elements:
+            sys.stdout.write(element)

d77740b3752bb30d8357f4d9a1faed4199fad7e2
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-19 12:51:15 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/python3
+import sys
+elements = []
+if __name__ == '__main__':
+
+
+    for line in sys.stdin:
+        lista=line.split(",")[1]
+
+        sys.stdout.write(line)
+            

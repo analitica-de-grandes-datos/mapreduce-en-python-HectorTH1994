1936aafc3df120ba40a2e08b69a595a4042b9b6a
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-23 23:05:00 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -4,16 +4,21 @@
 #! /usr/bin/python3
 
 import sys
+from operator import itemgetter, attrgetter
 elements=[]
 
 if __name__ == '__main__':
 
     def take_element(element):
         return int(element.split()[2])
+
+    def take_word(element):
+        return element.split()[0]
         
     for line in sys.stdin:
         elements.append(line)
     else:
-        elements = sorted(elements, key = take_element)
+        elements = sorted(elements, key = itemgetter(0,2))
+
         for element in elements:
             sys.stdout.write(element)

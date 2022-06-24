0a16e5db8797ddd3d869e4c002b5360759cbcb0b
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-24 06:34:02 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin/python3
+
+import sys
+
+elements=[]
+letras=[]
+elements2=[]
+contador=0
+
+if __name__ == '__main__':
+
+    def take_element(element):
+        return int(element.split()[2])
+
+    def take_word(element):
+        return element.split()[0]
+
+    for line in sys.stdin:
+        elements.append(line)
+
+    else:
+        elements = sorted(elements, key = take_element)
+
+        for element in elements:
+            if contador < 5:
+                sys.stdout.write(element)
+                contador+=1

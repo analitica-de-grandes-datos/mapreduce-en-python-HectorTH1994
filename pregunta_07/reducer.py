0264e75fcb3d49f61fc8005a4f4eaf18228be240
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-06-23 22:41:22 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin/python3
+
+import sys
+elements=[]
+
+if __name__ == '__main__':
+
+    def take_element(element):
+        return int(element.split()[2])
+        
+    for line in sys.stdin:
+        elements.append(line)
+    else:
+        elements = sorted(elements, key = take_element)
+        for element in elements:
+            sys.stdout.write(element)
